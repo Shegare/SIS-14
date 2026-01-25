8c4a635ccbc45471368a7af9b23e6fa9555843cb
Shegare <228shadowfox228@gmail.com>
2026-01-26 02:08:49 +0300

    fix time

--- Content.Shared/_SIS/Respawn/RespawnMemoryComponent.cs
+++ Content.Shared/_SIS/Respawn/RespawnMemoryComponent.cs
@@ -6,7 +6,7 @@ namespace Content.Shared._SIS.Respawn;
 public sealed partial class RespawnStatusComponent : Component
 {
     [DataField]
-    public TimeSpan TimeToUnlockRespawn = TimeSpan.FromSeconds(10); // TODO-SIS: Сделать 10 минут, а не секунд
+    public TimeSpan TimeToUnlockRespawn = TimeSpan.FromMinutes(10);
 
     [DataField, AutoNetworkedField]
     public TimeSpan TimeOfDeath;
