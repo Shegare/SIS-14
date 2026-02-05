e08b9a7a59dbf6681b83c9f4bebfc45c1132c49f
Shegare <228shadowfox228@gmail.com>
2026-02-05 05:28:45 +0300

    init

---
+++ Content.SIS.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using Robust.Client;
+
+namespace Content.SIS.Client;
+
+internal static class Program
+{
+    [STAThread]
+    public static void Main(string[] args)
+    {
+        ContentStart.Start(args);
+    }
+}
---
+++ Content.SIS.Server/Program.cs
@@ -0,0 +1,12 @@
+using Robust.Server;
+
+namespace Content.SIS.Server
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            ContentStart.Start(args);
+        }
+    }
+}
