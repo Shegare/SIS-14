4116de903629ced79f1e291c14e0932090bf0c0c
Shegare <228shadowfox228@gmail.com>
2026-02-10 21:52:31 +0300

    init C#

---
+++ Content.Module.Server/_Goobstation/NPC/HTN/HTNSystem.Goob.cs
@@ -0,0 +1,22 @@
+﻿using JetBrains.Annotations;
+
+namespace Content.Server.NPC.HTN;
+
+/// <summary>
+/// This handles...
+/// </summary>
+public sealed class GoobHTNSystem : EntitySystem
+{
+    [Dependency] private readonly HTNSystem _htn = default!;
+
+    // muh lacking of convenients overloads
+    [PublicAPI]
+    public void SetHTNEnabled(EntityUid uid, bool state, float planCooldown = 0f, HTNComponent? component = null)
+    {
+        if (!Resolve(uid, ref component))
+            return;
+
+        Entity<HTNComponent> ent = (uid, component);
+        _htn.SetHTNEnabled(ent, state, planCooldown);
+    }
+}
